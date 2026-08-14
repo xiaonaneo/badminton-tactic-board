9e1ef61a0a021ed3ca59fecbadb67f1314fb905f
xiaonaneo <neo111282@126.com>
2026-08-14 22:17:14 +0800

    Revert "优化侧边栏 UI"
    This reverts commit 99f462b8a02058d8b3299ffb5a76549e01138b99.

--- badminton-tactic-board/test/court-corners.test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('node:assert/strict');
-const fs = require('node:fs');
-const path = require('node:path');
-
-const court = fs.readFileSync(path.join(__dirname, '..', 'court.js'), 'utf8');
-assert.match(court, /const FLOOR_RADIUS\s*=\s*\d+/);
-assert.match(court, /rect\(0, 0, VIEW_W, VIEW_H, 'floor', \{ rx: FLOOR_RADIUS, ry: FLOOR_RADIUS \}\)/);
